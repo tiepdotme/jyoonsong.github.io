08e6e06b9af4ddc058def32980a6af1e90dae5b8
jyoonsong <jyo3on@gmail.com>
2019-05-04 02:30:14 +0000

    Travis #36

--- build.sh
+++ build.sh
@@ -24,7 +24,7 @@ cp -R ../jyoonsong.github.io/_site/* .
 # Make sure we have the updated .travis.yml file so tests won't run on master.
 cp ../jyoonsong.github.io/.travis.yml .
 git config user.email "jyo3on@gmail.com"
-git config user.name "jaeyo_on"
+git config user.name "jyoonsong"
 
 # Commit and push generated content to `master` branch.
 git status
